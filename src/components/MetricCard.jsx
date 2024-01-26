380d424a057c29f8731d30a78dd56d977a3db67c
Daniel Ochuba <danielochuba78@gmail.com>
2024-01-26 14:30:40 +0100

    add theme and menu items

---
+++ src/components/MetricCard.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import style from './MetricCard.module.css';
+
+const metricInfo = [
+    {
+        logo: '',
+        chart: '',
+        metric: '',
+        unit: '350',
+        description: 'Total Order',
+    },
+    {
+        logo: '',
+        chart: '',
+        metric: '',
+        unit: '',
+        description: '',
+    },
+    {
+        logo: '',
+        chart: '',
+        metric: '',
+        unit: '',
+        description: '',
+    },
+    {
+        logo: '',
+        chart: '',
+        metric: '',
+        unit: '',
+        description: '',
+    },
+]
+
+function MetricCard() {
+  return (
+    <div>
+        <div>
+            <img src={} alt='logo' />
+            <img src={} alt='chart' />
+        </div>
+        <div>
+            <p></p>
+            <p></p>            
+        </div>
+        <div>
+            
+        </div>      
+
+    </div>
+  )
+}
+
+export default MetricCard
